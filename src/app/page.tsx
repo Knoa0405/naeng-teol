4e1ddd8cac65545c663ee345bc6d10be400fd875
knoa0405 <knoa0405@gmail.com>
2024-08-14 20:55:28 +0900

    feat: add design theme in shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-// "use client";
 import { Button } from "@/components/ui/button";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
@@ -10,14 +9,15 @@ export default function Home() {
   async function run() {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const prompt = "한국어로 김치찌개 레시피 알려줘";
-
     const result = await model.generateContent(prompt);
-    const response = await result.response;
+    const response = result.response;
+
     const text = response.text();
     console.log(text);
   }
 
   run();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Click me</Button>
