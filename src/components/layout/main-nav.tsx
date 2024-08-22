c4dbf34ef4df209f22a0e8856e41bc2ee36549b7
knoa0405 <knoa0405@gmail.com>
2024-08-22 17:38:23 +0900

    feat: update nav

--- src/components/layout/main-nav.tsx
+++ src/components/layout/main-nav.tsx
@@ -8,8 +8,8 @@ const MainNav = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <nav className="flex justify-center items-center space-x-4 lg:space-x-6">
-      <Button>Click me</Button>
+    <nav className="flex justify-center items-center space-x-4 lg:space-x-6 pt-4">
+      <Button>냉장고 털기</Button>
       <Button
         variant="outline"
         size="icon"
@@ -17,7 +17,6 @@ const MainNav = () => {
       >
         <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <span className="sr-only">Toggle theme</span>
       </Button>
     </nav>
   );
