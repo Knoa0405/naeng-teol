b48da4595bfdca4b665f01be484fe0a41b2b9ed1
knoa0405 <knoa0405@gmail.com>
2024-10-09 00:29:24 +0900

    feat: update devtools middleware in zustand

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,35 +1,69 @@
+import { z } from "zod";
+import { create } from "zustand";
 import { RecipeSchema } from "@/types/schema";
 import { TIngredient, TIngredients } from "@/types";
-import { create } from "zustand";
-import { z } from "zod";
+import { devtools } from "zustand/middleware";
+import { isProduction } from "@/constants";
 
 type TRecipe = z.infer<typeof RecipeSchema>;
 interface IIngredientsStore {
   ingredients: TIngredients;
   addIngredient: (ingredient: TIngredient) => void;
-  removeIngredient: (ingredient: TIngredient) => void;
+  removeIngredient: (id: string) => void;
 }
 interface IRecipeStore {
   recipe: TRecipe;
   addRecipe: (recipe: TRecipe) => void;
 }
 
-export const useIngredientsStore = create<IIngredientsStore>((set) => ({
-  ingredients: [],
-  addIngredient: (ingredient) =>
-    set((state) => ({ ingredients: [...state.ingredients, ingredient] })),
-  removeIngredient: (ingredient) =>
-    set((state) => ({
-      ingredients: state.ingredients.filter((i) => i !== ingredient),
-    })),
-}));
+export const useIngredientsStore = create<IIngredientsStore>()(
+  devtools(
+    (set) => ({
+      ingredients: [],
+      addIngredient: (ingredient) => {
+        set(
+          () => ({
+            ingredients: Object.keys(ingredient).map((key) => ({
+              [key]: ingredient[key],
+            })),
+          }),
+          false,
+          "addIngredient"
+        );
+      },
+      removeIngredient: (id) =>
+        set(
+          (state) => ({
+            ingredients: state.ingredients.filter(
+              (ingredient) => Object.keys(ingredient).toString() !== id
+            ),
+          }),
+          false,
+          "removeIngredient"
+        ),
+    }),
+    { name: "ingredientsStore", enabled: !isProduction }
+  )
+);
 
-export const useRecipeStore = create<IRecipeStore>((set) => ({
-  recipe: {
-    content: "",
-  },
-  addRecipe: (recipe) =>
-    set(() => ({
-      recipe,
-    })),
-}));
+export const useRecipeStore = create<IRecipeStore>()(
+  devtools(
+    (set) => ({
+      recipe: {
+        content: "",
+      },
+      addRecipe: (recipe) =>
+        set(
+          () => ({
+            recipe,
+          }),
+          false,
+          "recipeStore"
+        ),
+    }),
+    {
+      name: "recipeStore",
+      enabled: !isProduction,
+    }
+  )
+);
