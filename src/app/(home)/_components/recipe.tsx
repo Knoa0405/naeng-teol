138d79387530d75e9138aaf922878f7e787c99e6
knoa0405 <knoa0405@gmail.com>
2024-10-09 00:28:05 +0900

    feat: update markdown recipe ui

--- src/app/(home)/_components/recipe.tsx
+++ src/app/(home)/_components/recipe.tsx
@@ -1,15 +1,38 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { useRecipeStore } from "@/store";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
+const headingStyles = {
+  h1: "text-2xl font-bold mt-6 mb-4",
+  h2: "text-xl font-semibold mt-5 mb-3",
+  h3: "text-lg font-medium mt-4 mb-2",
+};
+
 export const Recipe = () => {
   const recipe = useRecipeStore((state) => state.recipe);
-  console.log(recipe);
-  if (!recipe) {
-    return <span>Loading...</span>;
-  }
 
-  return <Markdown remarkPlugins={[remarkGfm]}>{recipe.content}</Markdown>;
+  return (
+    <Markdown
+      className={cn("flex flex-col p-4 rounded-sm max-w-72", {
+        "border border-green-600": !!recipe.content,
+      })}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        h1: ({ node, ...props }) => (
+          <h1 className={headingStyles.h1} {...props} />
+        ),
+        h2: ({ node, ...props }) => (
+          <h2 className={headingStyles.h2} {...props} />
+        ),
+        h3: ({ node, ...props }) => (
+          <h3 className={headingStyles.h3} {...props} />
+        ),
+      }}
+    >
+      {recipe.content}
+    </Markdown>
+  );
 };
