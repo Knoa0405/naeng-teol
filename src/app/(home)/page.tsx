6acceda343c88e85a4284c6b8da558cd07e62676
knoa0405 <knoa0405@gmail.com>
2024-11-04 20:46:09 +0900

    feat: suspense on recipe

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import IngredientsForm from "./_components/ingredients-form";
 import { Recipe } from "./_components/recipe";
 
@@ -6,7 +7,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col gap-4 px-4 py-8">
       <h1 className="text-4xl font-bold">냉장고 털기</h1>
       <IngredientsForm />
-      <Recipe />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Recipe />
+      </Suspense>
     </main>
   );
 }
