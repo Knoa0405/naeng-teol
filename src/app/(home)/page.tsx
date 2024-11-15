c4985223eb34305e59c853d17bd3194283cd72c2
knoa0405 <knoa0405@gmail.com>
2024-11-15 14:33:35 +0900

    feat: add loader in home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,14 @@
 import { Suspense } from "react";
 import IngredientsForm from "./_components/ingredients-form";
-import { Recipe } from "./_components/recipe";
+import Recipe from "./_components/recipe";
+import Loader from "./_components/loader";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-4 px-4 py-8">
-      <h1 className="text-4xl font-bold">냉장고 털기</h1>
+      <h1 className="text-4xl font-bold text-center">냉장고 털기</h1>
       <IngredientsForm />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Recipe />
       </Suspense>
     </main>
