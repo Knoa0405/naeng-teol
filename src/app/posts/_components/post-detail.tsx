87d1d49b4f02950439d1b70075fed40a5dfe0e74
knoa0405 <knoa0405@gmail.com>
2025-03-20 00:26:03 +0900

    refactor: update PostDetail component to fetch post data using id prop

--- src/app/posts/_components/post-detail.tsx
+++ src/app/posts/_components/post-detail.tsx
@@ -1,14 +1,15 @@
-"use client";
-
-import { IPost } from "@/types/posts";
+import { getPost } from "@/actions";
+import { IPostParams } from "@/types/posts";
 
 interface IPostDetailProps {
-  post: IPost;
+  id: IPostParams["id"];
 }
 
-export default function PostDetail({ post }: IPostDetailProps) {
+export default async function PostDetail({ id }: IPostDetailProps) {
+  const post = await getPost(id);
+
   return (
-    <div className="post-detail">
+    <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-4xl font-bold">{post.title}</h1>
       <ul>
         {post.ingredients.map((ingredient: any) => (
