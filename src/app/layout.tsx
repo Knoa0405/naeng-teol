ae5eea551a393a662b542b3ef24ee21fa1930a52
강노아 <knoa0405@gmail.com>
2025-04-01 21:04:06 +0900

    fix: update import path for Analytics component to use next version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import MainNav from "@/components/layout/main-nav";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 
 export const fetchCache = "default-cache";
 export const dynamic = "force-static";
