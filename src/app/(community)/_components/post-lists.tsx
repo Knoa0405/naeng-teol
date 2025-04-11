4a736a4046f9c48e2fa6758028ff460a47101fff
knoa0405 <knoa0405@gmail.com>
2025-04-11 18:47:59 +0900

    feat: implement revalidation for posts and refactor PostLists component to fetch posts directly

--- src/app/(community)/_components/post-lists.tsx
+++ src/app/(community)/_components/post-lists.tsx
@@ -1,12 +1,12 @@
-import { IPost } from "@/types/posts";
+import { getPosts } from "@/actions";
 
 import PostListItem from "./post-list-item";
 
-interface PostListsProps {
-  posts: IPost[];
-}
 
-const PostLists = ({ posts }: PostListsProps) => {
+const PostLists = async () => {
+  const postsData = await getPosts();
+  const posts = postsData.posts;
+
   return (
     <div className="mx-auto py-4">
       <div className="columns-2 gap-4 md:columns-2 lg:columns-3 xl:columns-4">
--- src/app/(community)/community/page.tsx
+++ src/app/(community)/community/page.tsx
@@ -1,15 +1,10 @@
-import { getPosts } from "@/actions";
-
 import PostLists from "../_components/post-lists";
 
 export default async function Community() {
-  const postsData = await getPosts();
-  const posts = postsData.posts;
-
   return (
     <section className="container flex min-h-screen flex-col gap-4 py-8">
       <h1 className="text-center text-4xl font-bold">커뮤니티</h1>
-      <PostLists posts={posts} />
+        <PostLists />
     </section>
   );
 }
