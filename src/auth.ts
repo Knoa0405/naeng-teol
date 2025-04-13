132d65f01f46a982865f147748c34b6ed56e0146
강노아 <knoa0405@gmail.com>
2025-04-14 02:08:12 +0900

    refactor: rearrange import statements in auth.ts and middleware.ts for better organization

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import prisma from "@/db";
-
-import authConfig from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
