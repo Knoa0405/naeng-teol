071e1002feb5f1d539bef09b3aa0342f15c21f8c
강노아 <knoa0405@gmail.com>
2025-04-13 16:55:19 +0900

    refactor: streamline database connection setup in index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,9 @@
-import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
 
 declare global {
   var prisma: PrismaClient | undefined;
