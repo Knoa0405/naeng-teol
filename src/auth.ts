0ba92a209e28de5aa0ace0d8c7570ab9a499e155
강노아 <knoa0405@gmail.com>
2025-04-15 20:02:57 +0900

    refactor: remove auth.config and integrate Google provider directly into auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,32 +1,15 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
 import prisma from "@/db";
 
-import authConfig from "./auth.config";
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt",
-  },
+  providers: [Google],
   callbacks: {
-    jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-
-      return token;
-    },
-    session({ session, token }) {
-      const id = token.sub;
-
-      if (id) {
-        session.user.id = id;
-      }
-
+    session({ session }) {
       return session;
     },
   },
-  ...authConfig,
 });
