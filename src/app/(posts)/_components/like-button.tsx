7831d5b653aa51fe5391059b733b4c72395dde43
강노아 <knoa0405@gmail.com>
2025-04-17 22:23:30 +0900

    refactor: update like button with useGetLike hook

--- src/app/(posts)/_components/like-button.tsx
+++ src/app/(posts)/_components/like-button.tsx
@@ -1,75 +1,32 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { Heart } from "lucide-react";
 
-import { postPostLike } from "@/actions";
-import { useToast } from "@/components/hooks/use-toast";
 import { Button } from "@/components/ui/button";
-import { api } from "@/lib/api-helper";
+
+import useGetLike from "../_hooks/use-get-like";
+
 interface LikeButtonProps {
   initialLikeCount: number;
   postId: string;
 }
 
 const LikeButton = ({ initialLikeCount, postId }: LikeButtonProps) => {
-  const { toast } = useToast();
   const [likeCount, setLikeCount] = useState(initialLikeCount);
-  const [liked, setLiked] = useState(false);
 
-  useEffect(() => {
-    const fetchLike = async () => {
-      // TODO: session 을 가져오려면 클라이언트에서 호출해야 하는데 서버에서 호출하면 안됨
-      // 이유는 클라이언트에서는 브라우저 쿠키를 사용하여 세션을 관리하기 때문에 서버액션이나 서버 컴포넌트 호출하면 쿠키를 가져오지 못하기 때문
-      // 서버 액션에서 인위적으로 쿠키를 넣어주는 방법도 있지만 아직은 클라이언트에서 호출하는 방법으로 해결
-      try {
-        const response = await api.get(`/api/posts/${postId}/like`);
-
-        const data = await response.json();
-
-        if (response.ok && data !== null) {
-          setLiked(true);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchLike();
-  }, [postId]);
+  const { liked, handlePostLike, handleDeleteLike } = useGetLike({ postId });
 
   const handleLike = async () => {
-    try {
-      const response = await postPostLike(Number(postId));
-
-      if (response.userId) {
-        setLiked(true);
-        setLikeCount(likeCount + 1);
-      }
-    } catch (error) {
-      toast({
-        variant: "destructive",
-        title: "좋아요 추가 실패",
-        description: "로그인 후 시도해주세요.",
-      });
+    if (liked) {
+      await handleDeleteLike();
+      setLikeCount(likeCount - 1);
     }
-  };
 
-  const handleUnlike = async () => {
-    try {
-      const response = await api.delete(`/api/posts/${postId}/like`);
-
-      if (response.ok) {
-        setLiked(false);
-        setLikeCount(likeCount - 1);
-      }
-    } catch (error) {
-      toast({
-        variant: "destructive",
-        title: "좋아요 취소 실패",
-        description:
-          error instanceof Error ? error.message : "좋아요 취소 실패",
-      });
+    if (!liked) {
+      await handlePostLike();
+      setLikeCount(likeCount + 1);
     }
   };
 
@@ -78,7 +35,7 @@ const LikeButton = ({ initialLikeCount, postId }: LikeButtonProps) => {
       variant="ghost"
       size="sm"
       className="flex items-center gap-1"
-      onClick={liked ? handleUnlike : handleLike}
+      onClick={handleLike}
     >
       <Heart
         className={`h-4 w-4 ${liked ? "fill-red-500 text-red-500" : ""}`}
---
+++ src/app/(posts)/_hooks/use-get-like.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+
+import { useToast } from "@/components/hooks/use-toast";
+import { api } from "@/lib/api-helper";
+
+interface IUseGetLikeProps {
+  postId: string;
+}
+
+const useGetLike = ({ postId }: IUseGetLikeProps) => {
+  const [liked, setLiked] = useState(false);
+  const { toast } = useToast();
+
+  useEffect(() => {
+    const getLike = async () => {
+      // TODO: session 을 가져오려면 클라이언트에서 호출해야 하는데 서버에서 호출하면 안됨
+      // 이유는 클라이언트에서는 브라우저 쿠키를 사용하여 세션을 관리하기 때문에 서버액션이나 서버 컴포넌트 호출하면 쿠키를 가져오지 못하기 때문
+      // 서버 액션에서 인위적으로 쿠키를 넣어주는 방법도 있지만 아직은 클라이언트에서 호출하는 방법으로 해결
+      try {
+        const response = await api.get(`/api/posts/${postId}/like`);
+        const data = await response.json();
+
+        if (response.ok && data !== null) {
+          setLiked(true);
+        }
+      } catch (error) {
+        toast({
+          variant: "destructive",
+          title: "좋아요 조회 실패",
+          description: "로그인 후 시도해주세요.",
+        });
+      }
+    };
+
+    getLike();
+  }, [postId, toast]);
+
+  const handlePostLike = async () => {
+    try {
+      const response = await api.post(`/api/posts/${postId}/like`);
+
+      if (response.ok) {
+        setLiked(true);
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "좋아요 추가 실패",
+        description: "로그인 후 시도해주세요.",
+      });
+    }
+  };
+
+  const handleDeleteLike = async () => {
+    try {
+      const response = await api.delete(`/api/posts/${postId}/like`);
+
+      if (response.ok) {
+        setLiked(false);
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "좋아요 취소 실패",
+        description: "로그인 후 시도해주세요.",
+      });
+    }
+  };
+
+  return { liked, handlePostLike, handleDeleteLike };
+};
+
+export default useGetLike;
