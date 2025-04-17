10180dd8605e1d24a9c6ae70b304a3879deeb3c0
강노아 <knoa0405@gmail.com>
2025-04-17 22:23:22 +0900

    feat: add error handling component for community page with retry functionality

--- src/app/(community)/community/page.tsx
+++ src/app/(community)/community/page.tsx
@@ -1,10 +1,16 @@
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+
+import CommunityError from "./error";
+
 import PostLists from "../_components/post-lists";
 
 export default function Community() {
   return (
     <section className="container flex min-h-screen flex-col gap-4 py-8">
       <h1 className="text-center text-4xl font-bold">커뮤니티</h1>
-      <PostLists />
+      <ErrorBoundary errorComponent={CommunityError}>
+        <PostLists />
+      </ErrorBoundary>
     </section>
   );
 }
