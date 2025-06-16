e3a0158cd47d085a9b7b2543925d7375d15576a3
강노아 <knoa0405@gmail.com>
2025-06-16 22:56:44 +0900

    chore: add bufferutil dependency and update session loading state styling

--- src/auth.ts
+++ src/auth.ts
@@ -7,9 +7,4 @@ import prisma from "@/db";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
-  callbacks: {
-    session({ session }) {
-      return session;
-    },
-  },
 });
